21072474b3fc26d2d214add012ac706b5ee80119
Nishant Malik <60120827+nishantmalik20@users.noreply.github.com>
2020-03-23 19:25:59 -0400

    Display Menu Added

--- calc.sh
+++ calc.sh
@@ -1,12 +1,21 @@
 #! /usr/bin/env bash
 
-# When the program is first loaded, display a greeting to the user.
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
+
+
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+
+# When the program is first loaded, display a greeting to the user.
 echo "Hi there, Welcome to the program."
+
+# Then, display a menu that outlines the possible operations:
+  # Add
+  # Subtract
+  # Exit
+
+echo "Make a Selection:"
+echo "Add"
+echo "Subtract"
+echo "Exit"
